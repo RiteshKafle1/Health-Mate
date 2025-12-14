a7ca1c58c31b954c802d71141c8df67e067412d0
Ritesh Kafle1 <riteshkafle10@gmail.com>
2025-12-14 14:19:15 +0545

    initialization of routes and services registering them to app

--- Health-Mate/app/main.py
+++ Health-Mate/app/main.py
@@ -1,15 +1,17 @@
 import logging
 from fastapi import FastAPI
 from app.core.database import connect_to_mongo,close_mongo_connection
+from app.routes.user_routes import router as user_router
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(levelname)s | %(message)s"
 )
+#creating the instance of fastapi
 app=FastAPI()
 
 # Register the every routes
-
+app.inlcude_router(user_router,prefix="/api/v1/users")
 
 # connecting to mongodb when the server starts
 @app.on_event("startup")
